540d57909f0833338b7d1faa365f5f1a31e1c07b
realityexpander <realityexpander@gmail.com>
2019-10-07 12:46:21 -0500

    A button clicks to sleep quality screen

--- TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -22,7 +22,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
 
 /**
@@ -46,6 +50,22 @@ class SleepQualityFragment : Fragment() {
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        val arguments = SleepQualityFragmentArgs.fromBundle(arguments!!)
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, dataSource)
+        val sleepQualityViewModel = ViewModelProviders.of(
+            this, viewModelFactory).get(SleepQualityViewModel::class.java)
+        binding.sleepQualityViewModel = sleepQualityViewModel
+        sleepQualityViewModel.navigateToSleepTracker.observe(this, Observer {
+            if (it == true) {
+                this.findNavController().navigate(
+                    SleepQualityFragmentDirections.actionSleepQualityFragmentToSleepTrackerFragment()
+                )
+                sleepQualityViewModel.doneNavigating()
+            }
+        })
+
 
         return binding.root
     }
--- TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ TrackMySleepQualityCoroutines-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -16,3 +16,42 @@
 
 package com.example.android.trackmysleepquality.sleepquality
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(
+      private val sleepNightKey: Long = 0L,
+      val database: SleepDatabaseDao): ViewModel() {
+  private val viewModelJob = Job()
+  private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+  private val _navigateToSleepTracker = MediatorLiveData<Boolean?>()
+  val navigateToSleepTracker: LiveData<Boolean?>
+    get() = _navigateToSleepTracker
+
+  fun doneNavigating() {
+    _navigateToSleepTracker.value = null
+  }
+
+  fun onSetSleepQuality(quality: Int) {
+    uiScope.launch {
+      withContext(Dispatchers.IO) {
+        val tonight = database.get(sleepNightKey) ?: return@withContext
+        tonight.sleepQuality = quality
+        database.update(tonight)
+      }
+
+      // Setting this state variable to true will alter thbe observer and trigger navigation
+      _navigateToSleepTracker.value = true
+    }
+  }
+
+  override fun onCleared() {
+    super.onCleared()
+    viewModelJob.cancel()
+  }
+}
