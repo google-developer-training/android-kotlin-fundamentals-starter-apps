0e4489ec5b7f530602d3418e7e07d1fcc4b253ec
Jhansi <24260141+jtavva@users.noreply.github.com>
2020-08-18 15:50:39 -0700

    ViewModelProviders is deprecated
    ViewModelProviders class is deprecated, code is updated to use ViewModelProvider constructors directly

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -1,12 +1,13 @@
 package com.example.android.gdgfinder.add
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
 
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.AddGdgFragmentBinding
@@ -31,7 +32,7 @@ import com.google.android.material.snackbar.Snackbar
 class AddGdgFragment : Fragment() {
 
     private val viewModel: AddGdgViewModel by lazy {
-        ViewModelProviders.of(this).get(AddGdgViewModel::class.java)
+        ViewModelProvider(this).get(AddGdgViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
