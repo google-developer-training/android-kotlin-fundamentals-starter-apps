2a4cb64a60da0a72a7598a37043b3a6841e42b09
yerdaulet1991 <yalibayev@nu.edu.kz>
2021-01-15 15:41:38 +0600

    finished lesson 10

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -22,7 +22,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -39,7 +42,15 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        binding.viewModel = viewModel
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { navigate ->
+                if(navigate) {
+                    val navController = findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
         return binding.root
     }
 }
