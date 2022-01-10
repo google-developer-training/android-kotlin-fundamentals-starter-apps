1ff9a074bc815f9f088de8da5ab5ef69d850d1d8
Tanmai2002 <tanmaikamat@gmail.com>
2022-01-10 20:33:30 +0530

    Updated dependecies and libraries

--- AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
