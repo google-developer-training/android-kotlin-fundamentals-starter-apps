4d695d6a1c186be9af20bcdf636b90a8331c7939
impactrudia <impactyoung@gmail.com>
2022-03-04 17:42:06 +0900

    feat : mochieconverter

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -21,23 +21,23 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
 interface MarsApiService {
-    @GET("relestate")
-    fun getProperties(): Call<String>
+    @GET("/realestate")
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 object MarsApi {
--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,9 +21,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,12 +49,12 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsRealEstateProperties() {
         MarsApi.retrofitService.getProperties().enqueue(
-            object : retrofit2.Callback<String> {
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _response.value = "Success: "+response.body()
+            object : retrofit2.Callback<List<MarsProperty>> {
+                override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                    _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
                 }
 
-                override fun onFailure(call: Call<String>, t: Throwable) {
+                override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                     _response.value = "Failure: " + t.message
                 }
             }
