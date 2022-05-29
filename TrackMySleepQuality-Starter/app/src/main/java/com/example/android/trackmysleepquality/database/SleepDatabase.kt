df277f8c59992b98197a236b615d9068c4528e3b
rodriguesporan <rodriguesporan@gmail.com>
2022-05-29 00:08:07 -0300

    Added SleepDatabase abstract class to create the contract to RoomDatabase instantiate the database and Daos

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,43 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(version = 1, entities = [SleepNight::class], exportSchema = false)
+abstract class SleepDatabase: RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        // The value of a volatile variable will never be cached,
+        // and all writes and reads will be done to and from the main memory.
+        // This helps make sure the value of INSTANCE is always up-to-date and the same to all execution threads.
+        // It means that changes made by one thread to INSTANCE are visible to all other threads immediately,
+        // and you don't get a situation where, say, two threads each update the same entity in a cache, which would create a problem.
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            // means that only one thread of execution at a time can enter this block of code,
+            // which makes sure the database only gets initialized once
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
