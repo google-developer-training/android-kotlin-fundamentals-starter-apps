056fc1675eb85394a03cb33f890d10f453f19b90
Alan Pazetto <alan.pazetto@profusion.mobi>
2023-01-04 16:12:01 -0300

    feat(DevByteViewModel): use VideosRepository in DevByteViewModel

--- RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ RepositoryPattern-Starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -23,9 +23,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.android.devbyteviewer.database.getDatabase
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
 import com.example.android.devbyteviewer.network.asDomainModel
+import com.example.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.*
 import java.io.IOException
 
@@ -44,25 +46,8 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
     /**
      * The data source this ViewModel will fetch results from.
      */
-    // TODO: Add a reference to the VideosRepository class
-
-    /**
-     * A playlist of videos displayed on the screen.
-     */
-    // TODO: Replace the MutableLiveData and backing property below to a reference to the 'videos'
-    // TODO: from the VideosRepository
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
-
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
+    private val videosRepository = VideosRepository(getDatabase(application))
+    val playlist = videosRepository.videos
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -94,26 +79,21 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        // TODO: Replace with a call to the refreshDataFromRepository9) method
-        refreshDataFromNetwork()
+        refreshDataFromRepository()
     }
 
-    /**
-     * Refresh data from the repository. Use a coroutine launch to run in a
-     * background thread.
-     */
-    // TODO: Replace with the refreshDataFromRepository() method
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = DevByteNetwork.devbytes.getPlaylist()
-            _playlist.postValue(playlist.asDomainModel())
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
 
-            _eventNetworkError.value = false
-            _isNetworkErrorShown.value = false
-
-        } catch (networkError: IOException) {
-            // Show a Toast error message and hide the progress bar.
-            _eventNetworkError.value = true
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if(playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
         }
     }
 
